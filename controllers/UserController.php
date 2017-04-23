70422a7f51d223560a8c7849133e9f7be8d58bc3
Akchapocha <poiskraboti@inbox.ru>
2017-04-23 13:29:19 +0300

    4.2

--- controllers/UserController.php
+++ controllers/UserController.php
@@ -5,3 +5,32 @@
  * Date: 23.04.2017
  * Time: 11:35
  */
+
+/**
+ * Подключаем модели
+ */
+require_once '../models/CategoriesModel.php';
+require_once '../models/UsersModel.php';
+
+/**
+ * AJAX регистрация пользователя
+ * Инициализация сессионной переменной ($_REQUEST['user'])
+ * @return json массив данных нового пользователя
+ */
+function registerAction()
+{
+    $email = isset($_REQUEST['email']) ? $_REQUEST['email'] : null;
+    $email = trim($email);
+
+    $pwd1 = isset($_REQUEST['pwd1']) ? $_REQUEST['pwd1'] : null;
+    $pwd2 = isset($_REQUEST['pwd2']) ? $_REQUEST['pwd2'] : null;
+
+    $phone = isset($_REQUEST['phone']) ? $_REQUEST['phone'] : null;
+    $adress = isset($_REQUEST['adress']) ? $_REQUEST['adress'] : null;
+    $name = isset($_REQUEST['name']) ? $_REQUEST['name'] : null;
+    $name = trim($name);
+
+    $resData = null;
+    $resData = checkRegisterParams($email, $pwd1, $pwd2);
+
+}
--- models/UsersModel.php
+++ models/UsersModel.php
@@ -5,3 +5,71 @@
  * Date: 23.04.2017
  * Time: 11:37
  */
+
+/**
+ * Регистрация нового пользователя
+ * 
+ * @param string $email почта
+ * @param string $pwdMD5 пароль зашифрованный в MD5
+ * @param string $name имя пользователя
+ * @param string $phone телефон
+ * @param string $adress адресс пользователя
+ */
+function registerNewUser($email, $pwdMD5, $name, $phone, $adress)
+{
+//    $email = htmlspecialchars(mysql_real_escape_string($email));
+//    $name = htmlspecialchars(mysql_real_escape_string(name));
+//    $phone = htmlspecialchars(mysql_real_escape_string($phone));
+//    $adress = htmlspecialchars(mysql_real_escape_string($adress));
+
+    $users = R::dispense('users');
+    $users->email = $email;
+    $users->pwd = $pwdMD5;
+    $users->name = $name;
+    $users->phone = $phone;
+    $users->adress = $adress;
+    R::store($users);
+
+
+    $sql = 'SELECT *
+        FROM `users`
+        WHERE (`email` = ' . $email . ' and `pwd` = ' . $pwdMD5 . ')
+        LIMIT 1';
+    if (isset(R::getAll($sql)[0])) {
+        R::getAll($sql)['success'] = 1;
+    } else {
+        R::getAll($sql)['success'] = 0;
+    }
+    return R::getAll($sql);
+}
+
+/**
+ * Проверка параметров для регистрации пользователя
+ *
+ * @param string $email
+ * @param string $pwd1
+ * @param string $pwd2
+ * @return array результат
+ */
+function checkRegisterParams($email, $pwd1, $pwd2)
+{
+    $res = null;
+
+    if (!$email){
+        $res['success'] = false;
+        $res['message'] = 'Введите email';
+    }
+    if (!$pwd1){
+        $res['success'] = false;
+        $res['message'] = 'Введите пароль';
+    }
+    if (!$pwd2){
+        $res['success'] = false;
+        $res['message'] = 'Введите повторный пароль';
+    }
+    if ($pwd1 != $pwd2){
+        $res['success'] = false;
+        $res['message'] = 'Пароли не совпадают';
+    }
+    return $res;
+}
